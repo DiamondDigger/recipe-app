ac450b3d12bc3b78aa3aa1ebe847113c7eb13127
naglec13 <naglec13@gmail.com>
2020-07-01 14:11:11 +0300

    adding check out of unit of measures

---
+++ src/main/java/guru/springframework/recipeapp/bootstrap/RecipeBootstrap.java
@@ -0,0 +1,77 @@
+package guru.springframework.recipeapp.bootstrap;
+
+import guru.springframework.recipeapp.domain.Recipe;
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import guru.springframework.recipeapp.repositories.CategoryRepository;
+import guru.springframework.recipeapp.repositories.RecipeRepository;
+import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RecipeBootstrap implements CommandLineRunner {
+
+    private RecipeRepository recipeRepository;
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public RecipeBootstrap(RecipeRepository recipeRepository, CategoryRepository categoryRepository,
+                           UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.recipeRepository = recipeRepository;
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    private List<Recipe> getRecipes() {
+        List<Recipe> recipes = new ArrayList<>(2);
+
+        //get UDMs
+        Optional<UnitOfMeasure> teaspoonUomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        if (!teaspoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+        Optional<UnitOfMeasure> tablespoonUomOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+        if (!tablespoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+        Optional<UnitOfMeasure> cupUomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        if (!cupUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+        Optional<UnitOfMeasure> pinchUomOptional = unitOfMeasureRepository.findByDescription("Pinch");
+        if (!pinchUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+        Optional<UnitOfMeasure> ounceUomOptional = unitOfMeasureRepository.findByDescription("Ounce");
+        if (!ounceUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+        Optional<UnitOfMeasure> eachUomOptional = unitOfMeasureRepository.findByDescription("Each");
+        if (!eachUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+        Optional<UnitOfMeasure> dashUomOptional = unitOfMeasureRepository.findByDescription("Dash");
+        if (!dashUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+        Optional<UnitOfMeasure> pintUomOptional = unitOfMeasureRepository.findByDescription("Pint");
+        if (!pinchUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Recipe spicyGrilledChickenTacos = new Recipe();
+        spicyGrilledChickenTacos.setDescription("");
+
+        spicyGrilledChickenTacos.setPrepTime(20);
+        spicyGrilledChickenTacos.setCookTime(15);
+
+    }
+}
