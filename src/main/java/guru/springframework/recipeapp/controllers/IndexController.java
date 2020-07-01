871cf7d1d9d3319c3909b897d04051ba3dc9a943
naglec13 <naglec13@gmail.com>
2020-07-01 17:58:55 +0300

    adding list of recipes to the view. Closes #9

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -1,33 +1,23 @@
 package guru.springframework.recipeapp.controllers;
 
-import guru.springframework.recipeapp.domain.Category;
-import guru.springframework.recipeapp.domain.UnitOfMeasure;
-import guru.springframework.recipeapp.repositories.CategoryRepository;
-import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
 
-        Optional<Category> optionalCategory = categoryRepository.findByDescription("Mexican");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Pinch");
-
-        System.out.println("Category ID is: " + optionalCategory.get().getId());
-        System.out.println("UDM ID is: " + unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
