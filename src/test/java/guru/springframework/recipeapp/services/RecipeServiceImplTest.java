e1cece52b1fa03c9e8d7e137eb1f72fe7ab1978a
naglec13 <naglec13@gmail.com>
2020-07-08 11:07:53 +0300

    added JUnit tests with Mockito to the getRecipe method.Closes #16

---
+++ src/test/java/guru/springframework/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.recipeapp.services;
+
+import guru.springframework.recipeapp.domain.Recipe;
+import guru.springframework.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+        assertEquals(recipeSet.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
