08b67e5c6e992831a002d2ebebdd25e035818f39
naglec13 <naglec13@gmail.com>
2020-07-11 12:54:55 +0300

    added recipe controller. Closes #25

---
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -0,0 +1,15 @@
+package guru.springframework.recipeapp.controllers;
+
+import guru.springframework.recipeapp.services.RecipeService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+}
