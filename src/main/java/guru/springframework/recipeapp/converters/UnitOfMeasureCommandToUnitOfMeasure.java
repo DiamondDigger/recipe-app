613f9ec7b9f960573b502f69f77216b89da1139b
naglec13 <naglec13@gmail.com>
2020-07-16 19:40:10 +0300

    added logic for UnitOfMeasure converts. Closes #39

--- src/main/java/guru/springframework/recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/guru/springframework/recipeapp/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -1,11 +1,25 @@
 package guru.springframework.recipeapp.converters;
 
+import com.sun.istack.Nullable;
 import guru.springframework.recipeapp.commands.UnitOfMeasureCommand;
 import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
 
-public class UnitOfMeasureCommandToUnitOfMeasure {
-    public UnitOfMeasure convert(UnitOfMeasureCommand o) {
-        UnitOfMeasure stub = new UnitOfMeasure();
-        return stub;
+
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand unitOfMeasureCommand) {
+        if (unitOfMeasureCommand != null) {
+            final UnitOfMeasure uom = new UnitOfMeasure();
+            uom.setId(unitOfMeasureCommand.getId());
+            uom.setDescription(unitOfMeasureCommand.getDescription());
+
+            return uom;
+        }
+        return null;
     }
 }
--- src/main/java/guru/springframework/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
+++ src/main/java/guru/springframework/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -1,11 +1,24 @@
 package guru.springframework.recipeapp.converters;
 
+import com.sun.istack.Nullable;
 import guru.springframework.recipeapp.commands.UnitOfMeasureCommand;
 import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
 
-public class UnitOfMeasureToUnitOfMeasureCommand {
-    public UnitOfMeasureCommand convert(UnitOfMeasure o) {
-        UnitOfMeasureCommand stub = new UnitOfMeasureCommand();
-        return stub;
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+        if (unitOfMeasure != null) {
+            UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(unitOfMeasure.getId());
+            uomc.setDescription(unitOfMeasure.getDescription());
+            return uomc;
+        }
+
+        return null;
     }
 }
