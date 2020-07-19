dd4c257fe5b2df9888b7e789eb57e19a02497872
naglec13 <naglec13@gmail.com>
2020-07-19 23:59:49 +0300

    added save or update mapping in Recipe Controller. Closes - #48

--- src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -4,7 +4,9 @@ import guru.springframework.recipeapp.commands.RecipeCommand;
 import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -31,4 +33,15 @@ public class RecipeController {
 
         return "recipe/recipeForm";
     }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+       RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/showPage/" + savedRecipeCommand.getId();
+    }
+
+
+
 }
